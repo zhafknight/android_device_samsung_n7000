3515641a5c94c069f6c4818987fe93cee83fda9c
Dominggoes Isakh <drjisakh@gmail.com>
2020-11-16 22:51:08 +0100

    i9100: Use TARGET_SYSTEM_PROP for system.prop
    Somehow in R it doesn't merge into /system/build.prop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,5 +39,8 @@ endif
 # assert
 TARGET_OTA_ASSERT_DEVICE := galaxys2,i9100,GT-I9100,GT-I9100M,GT-I9100P,GT-I9100T,SC-02C
 
+# Properties
+TARGET_SYSTEM_PROP += device/samsung/i9100/system.prop
+
 # Use the non-open-source parts, if they're present
 -include vendor/samsung/i9100/BoardConfigVendor.mk
