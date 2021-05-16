7ea89e5ad59faae205d441fe88c3ffc26d9bcbdc
Dominggoes Isakh <drjisakh@gmail.com>
2021-05-16 20:29:58 +0200

    Revert "i9100: Configure for GO mini"
    This reverts commit bbd0ad6ec4326a211f616de821fe4572c3edda2c.

--- lineage_i9100.mk
+++ lineage_i9100.mk
@@ -23,7 +23,7 @@ TARGET_SCREEN_WIDTH := 480
 TARGET_BOOTANIMATION_HALF_RES := true
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_mini_go_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
