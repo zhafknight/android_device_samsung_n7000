266827cf90ece92328915ac6c1c7132887e92f8a
zhafknight <96339831+zhafknight@users.noreply.github.com>
2024-12-17 21:02:45 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,28 +19,23 @@
 
 -include device/samsung/galaxys2-common/BoardCommonConfig.mk
 
-TARGET_BOARD_INFO_FILE := device/samsung/i9100/board-info.txt
+TARGET_BOARD_INFO_FILE := device/samsung/n7000/board-info.txt
 
 # Device specific headers
-TARGET_SPECIFIC_HEADER_PATH += device/samsung/i9100/include
+TARGET_SPECIFIC_HEADER_PATH += device/samsung/n7000/include
 
 # Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9100/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/n7000/bluetooth
 
 # Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
-
-ifeq ($(WITH_MAGISKRAMDISK),true)
-TARGET_KERNEL_CONFIG := lineageos_i9100_magisk_defconfig
-else
-TARGET_KERNEL_CONFIG := lineageos_i9100_defconfig
-endif
+TARGET_KERNEL_CONFIG := lineageos_n7000_defconfig
 
 # assert
-TARGET_OTA_ASSERT_DEVICE := galaxys2,i9100,GT-I9100,GT-I9100M,GT-I9100P,GT-I9100T,SC-02C
+TARGET_OTA_ASSERT_DEVICE := galaxynote,n7000,N7000,GT-N7000
 
 # Properties
-TARGET_SYSTEM_PROP += device/samsung/i9100/system.prop
+TARGET_SYSTEM_PROP += device/samsung/n7000/system.prop
 
 # Use the non-open-source parts, if they're present
--include vendor/samsung/i9100/BoardConfigVendor.mk
+-include vendor/samsung/n7000/BoardConfigVendor.mk
