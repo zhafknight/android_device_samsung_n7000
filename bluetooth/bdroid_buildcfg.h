8d495b748a9dbb75684fc5c21c8e9be1a147a4ff
zhafknight <96339831+zhafknight@users.noreply.github.com>
2024-12-17 21:52:37 +0800

    Update bdroid_buildcfg.h

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "GT-I9100"
+#define BTM_DEF_LOCAL_NAME   "GT-N7000"
 
 /* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
 #define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
