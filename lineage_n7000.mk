b8fb02ff617bb1eb5a3dbacc41e3a67a678e62d4
IRonSquidist <96339831+zhafknight@users.noreply.github.com>
2026-08-06 10:50:20 +0800

    n7000: Set stock build fingerprint

--- lineage_n7000.mk
+++ lineage_n7000.mk
@@ -36,10 +36,6 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-N7000
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    DeviceName=GT-N7000 \
-    BuildDesc="GT-N7000-user 4.1.2 JZO54K N7000XXLSZ release-keys" \
-    BuildFingerprint=samsung/GT-N7000/GT-N7000:4.1.2/JZO54K/N7000XXLSZ:user/release-keys
-    
+BUILD_FINGERPRINT := "samsung/GT-N7000/GT-N7000:4.1.2/JZO54K/N7000XXLSZ:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
