3ab40792c2c6b79ae04d6a03a336d4564ebc8db6
IRonSquidist <96339831+zhafknight@users.noreply.github.com>
2026-08-06 10:50:20 +0800

    n7000: libsensors: Include libhardware headers

--- packages/libsensors/Android.mk
+++ packages/libsensors/Android.mk
@@ -23,6 +23,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM)
 
+LOCAL_HEADER_LIBRARIES += libhardware_headers
+
 LOCAL_MODULE_RELATIVE_PATH := hw
 
 LOCAL_MODULE_TAGS := optional
