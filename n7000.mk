02f366cf8b626ebcc2d5ae0977dcde5f1d2913c1
zhafknight <zhafmazran@gmail.com>
2026-08-29 11:59:16 +0800

    n7000: drop hdpi and xhdpi AAPT configs

--- n7000.mk
+++ n7000.mk
@@ -27,7 +27,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.target.rc
 
 # This device is xhdpi.
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 PRODUCT_PROPERTY_OVERRIDES += \
